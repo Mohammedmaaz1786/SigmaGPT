673324fa2add7889f7f3a0b4078398db6cda0d4a
Mohammedmaaz1786 <maazkhaleel17@gmail.com>
2026-06-15 20:34:09 +0530

    feat: add dotenv for environment variable management and connect to MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 8080;
@@ -10,7 +11,18 @@ app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
+
+const connectDB=async()=>{
+  try
+  {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
 
 // app.post("/test", async (req, res) => {
 
